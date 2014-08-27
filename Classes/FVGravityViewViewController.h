a312ce90537ff23ffd77802f41737ef52cc25b98
Fernando Valente <fernando@fvalente.org>
2014-08-27 21:27:20 +0300

    Create FVGravityViewViewController.h

---
+++ Classes/FVGravityViewViewController.h
@@ -0,0 +1,17 @@
+//
+//  FVGravityViewViewController.h
+//  FVGravityView
+//
+//  Created by Fernando Valente on 3/21/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FVGravityView.h"
+
+@interface FVGravityViewViewController : UIViewController {
+	FVGravityView *gravityView;
+}
+
+@end
+
